4a0fef5031a831c2b7ea53e033a6b72731e836a6
Dawid Polok <poldave111@gmail.com>
2023-03-19 16:56:32 +0100

    Fix model create error

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,22 +3,42 @@ const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
     try {
-        const { login, password } = req.body;
+        const { login, password } = req.body; 
         if (login && typeof login === 'string' && password && typeof password === 'string') {
             const userWithLogin = await User.findOne({ login });
-            if(userWithLogin) {
-                res.status(409).send({ message: 'User already logged in.' });
+            if (userWithLogin) {
+                return res.status(400).send({ message: 'User whit this login is already registered.'});
             }
+    
             const user = await new User.create({ login, password: await bcrypt.hash(password, 10) });
-            res.status(201).send({ message: 'User added' + user.login});
-        } else {
-            res.status(400).send({ message: 'Bad request' });
+            res.status(201).send({ message: 'User created' + user.login });
+    
+        }  else {
+            res.status(400).send({message: 'Bad request'});
         }
-    } catch (e) {
-        res.status(500).send({ message: e.message }); 
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
 }
 
 exports.login = async (req, res) => {
-
+    try {
+        const { login, password } = req.body; 
+        if (login && typeof login === 'string' && password && typeof password === 'string') {
+            const user = await User.findOne({ login });
+            if (!user) {
+                res.status(400).send({ message: 'Login or password are incorrect' });
+            }
+            else {
+                if (bcrypt.compareSync(password, user.password)) {
+                    res.status(200).send({ message: 'Login successful' });
+                }
+                else {
+                    res.status(400).send({ message: 'Login or password are incorrect'});
+                }
+            }
+        }
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
 }
