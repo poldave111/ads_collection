181e1411ddb49e751092731753a04d3cf7fc094e
Dawid Polok <poldave111@gmail.com>
2023-04-13 18:52:54 +0200

    Fix change in config.js path

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
 
-export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/ads' : 'http://localhost:8000/ads';
+export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/uploads' : 'http://localhost:8000/uploads';
