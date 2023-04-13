4fdae5180cea02c19d15af4d419afb5a36606627
Dawid Polok <poldave111@gmail.com>
2023-04-13 15:29:42 +0200

    Fix process.env added (config.js)

--- client/src/components/Ad/Ad.js
+++ client/src/components/Ad/Ad.js
@@ -2,13 +2,14 @@ import { useSelector } from "react-redux";
 import { Form, Col, Button } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import styles from './Ad.module.scss';
+import { IMAGES_URL } from '../../config';
 
 const  Ad = (props) => {
 
     return (
         <div className={styles.card}>
             <li>Title: {props.ad.title}</li>
-            <li>Image: <img src={`/uploads/${props.ad.image}`} /></li>
+            <li>Image: <img src={`${IMAGES_URL}/${props.ad.image}`} /></li>
             <Link to="/adview"><Button>More</Button></Link>
         </div>
     )
